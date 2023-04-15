06f42f6fec4a0a9c1be99275f810d9984ec9591d
Isaac Batista <isaacbatst@gmail.com>
2023-04-15 14:26:49 -0300

    :lipstick: improve header style

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Header: React.FC = (props: Props) => {
   return (
-    <header className='py-3 px-5'>
+    <header className='py-3 px-5 absolute top-0 bg-theme-yellow-500 w-full'>
       <h1 className='text-2xl font-bold'>Criadores e Afiliados</h1>
     </header>
   )
