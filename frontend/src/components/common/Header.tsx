3bfda38eec80cd1ded1774af62b169acdc3fef39
Isaac Batista <isaacbatst@gmail.com>
2023-04-16 22:17:46 -0300

    :lipstick: increase header spacing

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header: React.FC = () => {
   return (
-    <header className='py-3 px-5 bg-slate-900'>
+    <header className='py-5 px-5 bg-slate-900'>
       <Link href='/'>
         <h1 className='text-2xl font-bold text-white'>Criadores e Afiliados</h1>
       </Link>
