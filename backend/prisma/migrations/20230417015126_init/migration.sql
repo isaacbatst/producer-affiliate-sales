281a772f47c36f0810e58e431efbe38b6b999576
Isaac Batista <isaacbatst@gmail.com>
2023-04-16 22:52:19 -0300

    :heavy_plus_sign: install prisma

---
+++ backend/prisma/migrations/20230417015126_init/migration.sql
@@ -0,0 +1,60 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('CREATOR_SELL', 'AFFILIATE_SELL', 'COMMISSION_PAYMENT', 'COMMISION_RECEIVEMENT');
+
+-- CreateTable
+CREATE TABLE "Seller" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+
+    CONSTRAINT "Seller_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transactions" (
+    "id" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "value" INTEGER NOT NULL,
+    "productId" TEXT NOT NULL,
+    "sellerId" TEXT NOT NULL,
+
+    CONSTRAINT "Transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_affiliates" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_affiliates_AB_unique" ON "_affiliates"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_affiliates_B_index" ON "_affiliates"("B");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "Seller"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "Transactions_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "Transactions_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "Seller"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_affiliates" ADD CONSTRAINT "_affiliates_A_fkey" FOREIGN KEY ("A") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_affiliates" ADD CONSTRAINT "_affiliates_B_fkey" FOREIGN KEY ("B") REFERENCES "Seller"("id") ON DELETE CASCADE ON UPDATE CASCADE;
