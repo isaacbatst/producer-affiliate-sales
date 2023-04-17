59175471247c8ded8030b3c2ecd1facc7d6a4523
Isaac Batista <isaacbatst@gmail.com>
2023-04-16 22:05:14 -0300

    :speech_balloon: update section title

--- frontend/src/components/Home/TransactionsList.tsx
+++ frontend/src/components/Home/TransactionsList.tsx
@@ -10,7 +10,7 @@ const TransactionsList = ({isLoading, transactions}: Props) => {
   return (
     <section aria-label='Transações' className='bg-slate-900 py-10 flex-1'>
       <div className="container flex flex-col items-center mx-auto text-white px-5 md:px-0">
-        <h2 className='text-xl lg:text-4xl mb-10 font-bold' id='transactions-list-title'>Lista de Transações</h2>
+        <h2 className='text-xl lg:text-4xl mb-10 font-bold' id='transactions-list-title'>Transações</h2>
         {isLoading && <p role='status'>Carregando...</p>}
         {transactions && <ul className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4' aria-labelledby='transactions-list-title'>
           {transactions?.map(transaction => <Transaction transaction={transaction} key={transaction.id} />)}
